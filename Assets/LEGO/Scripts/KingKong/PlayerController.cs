4746243a6c00b955b4b9597238814de6a7917acd
LeviHerring <514121@escg.ac.uk>
2022-01-21 16:11:18 +0000

    King Kong stuff

--- Assets/LEGO/Scripts/KingKong/PlayerController.cs
+++ Assets/LEGO/Scripts/KingKong/PlayerController.cs
@@ -1,18 +1,60 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float movementSpeed = 50.0f;
+
+    public float jumpHeight = 15.0f;
+
+    public bool isGrounded = true; 
+
     void Start()
     {
-        
+        BoxCollider collider = GetComponent<BoxCollider>();
+        isGrounded = true; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.position += Vector3.back * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.position += Vector3.up * jumpHeight;
+            isGrounded = false; 
+        }
+
+       
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Platform")
+        {
+            isGrounded = true; 
+        }
+    }
+
 }
