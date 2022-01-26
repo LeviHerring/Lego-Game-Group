984f436b5dec7a2ad905bd92b40a119aa2e5c70d
JackBaker1 <163625@chrome.escg.ac.uk>
2022-01-26 15:47:43 +0000

    GACK HAS ARRIVED

---
+++ Assets/LEGO/Scripts/KingKong/gacksPlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class gacksPlayerMovement: MonoBehaviour
+{
+
+    public float playerSpeed;
+    public float walkSpeed = 0.0000000000000001f;
+    public float mouseSensitivity = 2f;
+    public float jumpHeight = 3f;
+    private bool isMoving = false;
+    private bool isSprinting = false;
+    private float yRot;
+
+    private Animator anim;
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playerSpeed = walkSpeed;
+        anim = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
+
+        isMoving = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed);
+            rigidBody.velocity += transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed;
+            isMoving = true;
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * playerSpeed);
+            rigidBody.velocity += transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed;
+            isMoving = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * jumpHeight);
+        }
+
+        
+    }
+}
