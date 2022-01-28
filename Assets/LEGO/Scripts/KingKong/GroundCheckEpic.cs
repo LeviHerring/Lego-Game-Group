4b07a4e5c93fc8a3bad0031c9bfa260eafd43269
JackBaker1 <163625@chrome.escg.ac.uk>
2022-01-28 16:12:47 +0000

    Almost finished playermovement, just need a couple of changes to jumping mechanic

---
+++ Assets/LEGO/Scripts/KingKong/GroundCheckEpic.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckEpic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+            GetComponent<gacksEpicPM>().isGrounded = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<gacksEpicPM>().isGrounded = false;
+    }
+}
+
